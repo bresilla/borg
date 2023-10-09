9bff742d61c836cfa5041bac41a61d7323124c26
Trim Bresilla <trim.bresilla@gmail.com>
2023-10-09 23:35:07 +0200

    feat: Update command line interface options in builder.rs
    - Modify `cli` function in `builder.rs` to include a logo when the `logo` parameter is true
    - Add logos for the `cli` function's command line interface
    - Modify `styles` variable in the `cli` function to include different styling options
    - Add `author` field for the `cli` function's command line interface
    - Modify `long_about` field for the `cli` function's command line interface
    - Modify `subcommand` field for the `cli` function's command line interface, adding aliases and changing the `about` field
    - Add `subcommand` field for the `cli` function's command line interface, with various options including `--csv`, `--field`, and `--no_lost_messages`

--- src/cli/builder.rs
+++ src/cli/builder.rs
@@ -489,7 +489,7 @@ pub fn cli(subs: bool, logo: bool) -> Command {
         )
         .subcommand(
             Command::new("interface")
-                .about(aly("i", subs) + "Various interface subcommands\n")
+                .about(aly("i", subs) + "Various interface subcommands")
                 .aliases(&["i", "int"])
                 .subcommand_required(true)
                 .arg_required_else_help(true)
@@ -547,6 +547,7 @@ pub fn cli(subs: bool, logo: bool) -> Command {
         )
 
 
+
         .subcommand(
             Command::new("run")
                 .about(aly("r", subs) + "Run an executable")
@@ -595,60 +596,62 @@ pub fn cli(subs: bool, logo: bool) -> Command {
         )
 
         .subcommand(
-            Command::new("pkg")
-                .about("Various package subcommands")
+            Command::new("workspace")
+                .about(aly("w", subs) + "Various workspace subcommands")
+                .aliases(&["w", "pkg", "pack"])
                 .subcommand_required(true)
                 .arg_required_else_help(true)
                 .subcommand(
                     Command::new("create")
                     .about("Create a new package")
+                    .aliases(["c", "new"])
                     .arg_required_else_help(true)
+                    .arg(
+                        arg!(<PACKAGE_NAME> "Name of the ROS package to create (e.g. 'demo_nodes_cpp')")
+                        .required(true)
+                    )
+                    .arg(arg!(--package_format <PACKAGE_FORMAT> "The package.xml format."))
+                    .arg(arg!(--description <DESCRIPTION> "The description given in the package.xml"))
+                    .arg(arg!(--license <LICENSE> "The license attached to this package"))
+                    .arg(arg!(--destination_directory <DESTINATION_DIRECTORY> "Directory where to create the package directory"))
+                    .arg(arg!(--build_type <BUILD_TYPE> "The build type to process the package with"))
+                    .arg(arg!(--dependencies <DEPENDENCIES>... "List of dependencies"))
+                    .arg(arg!(--maintainer_email <MAINTAINER_EMAIL> "Email address of the maintainer of this package"))
+                    .arg(arg!(--maintainer_name <MAINTAINER_NAME> "Name of the maintainer of this package"))
+                    .arg(arg!(--node_name <NODE_NAME> "Name of the empty executable"))
+                    .arg(arg!(--library_name <LIBRARY_NAME> "Name of the empty library"))
                 )
                 .subcommand(
                     Command::new("list")
                     .about("List all packages")
+                    .aliases(["l", "ls"])
                     .arg_required_else_help(true)
+                    .arg(arg!(-a --all "Display all packages even hidden ones"))
+                    .arg(arg!(-c --count_packages "Only display the number of packages discovered"))
                 )
                 .subcommand(
                     Command::new("info")
                     .about("Print information about a package")
+                    .aliases(["i", "show"])
                     .arg_required_else_help(true)
-                )
-                .subcommand(
-                    Command::new("prefix")
-                    .about("Print the prefix path of a package")
-                    .arg_required_else_help(true)
-                )
-                .subcommand(
-                    Command::new("share")
-                    .about("Print the share path of a package")
-                    .arg_required_else_help(true)
-                )
-                .subcommand(
-                    Command::new("executables")
-                    .about("Print the executables of a package")
-                    .arg_required_else_help(true)
-                )
-                .subcommand(
-                    Command::new("dependencies")
-                    .about("Print the dependencies of a package")
-                    .arg_required_else_help(true)
-                )
-                .subcommand(
-                    Command::new("xml")
-                    .about("Print the package.xml of a package")
-                    .arg_required_else_help(true)
+                    .arg(
+                        arg!(<PACKAGE_NAME> "Name of the ROS package to get info (e.g. 'demo_nodes_cpp')")
+                        .required(true)
+                    )
+                    .arg(arg!(--xml "Output the XML of the package manifest"))
                 )
                 .subcommand(
                     Command::new("build")
                     .about("Build a package")
+                    .aliases(["b", "make", "m"])
                     .arg_required_else_help(true)
                 )
         )
 
         .subcommand(
             Command::new("bag")
-                .about("Various rosbag subcommands")
+                .about(aly("b", subs) + "Various rosbag subcommands")
+                .aliases(&["b"])
                 .subcommand_required(true)
                 .arg_required_else_help(true)
         )
