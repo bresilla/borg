52d2960644736e35820ddd487d99850274d4db1f
Trim Bresilla <trim.bresilla@gmail.com>
2023-10-09 21:00:46 +0200

    feat: Add new subcommands and improve documentation for `alli` tool
    - Added various subcommands to the `alli` tool for listing available topics, displaying bandwidth, delay, and other operations related to topics, interfaces, parameters, nodes, services, packages, executables, dependencies, launch files, and rosbags.
    - Updated the `README.md` file by removing unnecessary ASCII art.

--- src/cli/builder.rs
+++ src/cli/builder.rs
@@ -485,7 +485,6 @@ pub fn cli(subs: bool, logo: bool) -> Command {
                     .arg(arg!(--spin_time <SPIN_TIME> "Spin time for discovery (if daemon not used)"))
                     .arg(arg!(-a --all "Display all nodes even hidden ones"))                                                                                                                                                                                                    
                     .arg(arg!(-c --count_nodes "Only display the number of nodes discovered"))
-
                 )
         )
         .subcommand(
@@ -497,27 +496,53 @@ pub fn cli(subs: bool, logo: bool) -> Command {
                 .subcommand(
                     Command::new("list")
                     .about("List all interface types available")
+                    .aliases(["l", "ls"])
                     .arg_required_else_help(true)
+                    .arg(arg!(-m --only_msgs "Print out only the message types"))
+                    .arg(arg!(-s --only_srvs "Print out only the service types"))
+                    .arg(arg!(-a --only_actions "Print out only the action types"))
                 )
                 .subcommand(
                     Command::new("package")
                     .about("Output a list of available interface types within one package")
+                    .aliases(["p", "pkg"])
                     .arg_required_else_help(true)
+                    .arg(
+                        arg!(<PACKAGE_NAME> "Name of the ROS package to get info (e.g. 'std_msgs')")
+                        .required(true)
+                    )
                 )
                 .subcommand(
-                    Command::new("packages")
+                    Command::new("all")
                     .about("Output a list of available interface types within all packages")
+                    .aliases(["a", "packages"])
                     .arg_required_else_help(true)
+                    .arg(arg!(-m --only_msgs "Print out only the message types"))
+                    .arg(arg!(-s --only_srvs "Print out only the service types"))
+                    .arg(arg!(-a --only_actions "Print out only the action types"))
                 )
                 .subcommand(
                     Command::new("show")
                     .about("Show the interface definition for a given type")
+                    .aliases(["s", "info"])
                     .arg_required_else_help(true)
+                    .arg(
+                        arg!(<TYPE> "Show an interface definition (e.g. 'example_interfaces/msg/String'). Passing '-' reads the argument from stdin (e.g. 'ros2 topic type /chatter | ros2 interface show -').")
+                        .required(true)
+                    )
+                    .arg(arg!(--all_comments "Show all comments, including for nested interface definitions"))
+                    .arg(arg!(--no_comments "Show no comments or whitespace"))
                 )
                 .subcommand(
-                    Command::new("proto")
-                    .about("Output an interface prototype")
-                    .arg_required_else_help(true)
+                    Command::new("model")
+                    .about("Output an interface model/prototype")
+                    .aliases(["m", "prototype", "proto"])
+                    .arg_required_else_help(true)                                                                                                                                                                                                                      
+                    .arg(
+                        arg!(<TYPE> "Show an interface definition (e.g. 'example_interfaces/msg/String')")
+                        .required(true)
+                    )
+                    .arg(arg!(--no_quotes "if true output has no outer quotes"))               
                 )
         )
 
