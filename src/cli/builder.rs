da9c4f7efdd2e83e7c8bf957de73b27234bc64fd
Trim Bresilla <trim.bresilla@gmail.com>
2023-10-09 23:49:06 +0200

    feat: Add new subcommand <borg frame>
    - Added new subcommands to the CLI: `frame`, `echo`, `list`, `info`, `workspace`, `create`, `build`, and `bag`
    - Implemented options for exporting frame tree to different file formats
    - Improved functionality and usability of the CLI overall

--- src/cli/builder.rs
+++ src/cli/builder.rs
@@ -545,7 +545,57 @@ pub fn cli(subs: bool, logo: bool) -> Command {
                     .arg(arg!(--no_quotes "if true output has no outer quotes"))               
                 )
         )
-
+        .subcommand(
+            Command::new("frame")
+            .about(aly("f", subs) + "Various transforms subcommands [WIP]\n")
+            .aliases(&["f", "tf"])
+            .subcommand_required(true)
+            .arg_required_else_help(true)
+            .subcommand(
+                Command::new("echo")
+                .about("Print transforms from the tree to screen")
+                .aliases(["e", "cat"])
+                .arg_required_else_help(true)
+                .arg(
+                    arg!(<TARGET_FRAME> "Name of the target frame (e.g. 'base_link')")
+                    .required(true)
+                )
+                .arg(
+                    arg!(<SOURCE_FRAME> "Name of the source frame (e.g. 'odom')")
+                    .required(true)
+                )
+                .arg(arg!(-s --use_sim_time "Use simulation time if available"))
+                .arg(arg!(--no_daemon "Don't spawn or use running daemon"))
+                .arg(arg!(--spin_time <SPIN_TIME> "Time (sec) to spin. Default: until interrupted"))
+                .arg(arg!(--rate <RATE> "Rate at which to display transforms. Default: 10.0"))
+                .arg(arg!(--once "Print first message and exit"))
+            )
+            .subcommand(
+                Command::new("list")
+                .about("Output a list of available frames")
+                .aliases(["l", "ls"])
+                .arg_required_else_help(true)
+                .arg(arg!(-a --all "Display all frames even hidden ones"))
+                .arg(arg!(-c --count_frames "Only display the number of frames discovered"))
+            )
+            .subcommand(
+                Command::new("info")
+                .about("Print information about a frame")
+                .aliases(["i", "show"])
+                .arg_required_else_help(true)
+                .arg(
+                    arg!(<FRAME_NAME> "Name of the frame to get info (e.g. 'base_link')")
+                    .required(true)
+                )
+                .arg(arg!(--spin_time <SPIN_TIME> "Spin time for discovery (if daemon not used)"))
+                .arg(arg!(--no_daemon "Don't spawn or use a running daemon"))
+                .arg(arg!(--include_hidden_services "Include hidden services"))
+                .arg(arg!(--export_dot <EXPORT_DOT> "Export the frame tree to a dot file").conflicts_with("export_json").conflicts_with("export_yaml"))
+                .arg(arg!(--export_json <EXPORT_JSON> "Export the frame tree to a json file").conflicts_with("export_dot").conflicts_with("export_yaml"))
+                .arg(arg!(--export_yaml <EXPORT_YAML> "Export the frame tree to a yaml file").conflicts_with("export_dot").conflicts_with("export_json"))
+                .arg(arg!(--export_image <EXPORT_IMAGE> "Export the frame tree to an image file"))
+            )
+        )
 
 
         .subcommand(
