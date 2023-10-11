72647cded92f5ad158d03d9f9040b03d658913d0
Trim Bresilla <trim.bresilla@gmail.com>
2023-10-11 20:38:10 +0200

    feat: Finished <borg action info>
    - Added a new function `run_command` to handle running a command based on given arguments
    - Updated `handle` function to use the new `run_command` function
    - Implemented `info` subcommand that calls the `info::handle` function
    - Implemented `list` subcommand that calls the `list::handle` function
    - Added new arguments to the `info` and `list` subcommands for additional functionality
    - Updated README file by removing unnecessary whitespace

--- src/cli/action.rs
+++ src/cli/action.rs
@@ -1,4 +1,4 @@
-use clap::{arg, Command};
+use clap::{arg, Command, Arg, ArgAction};
 
 pub fn cmd() -> Command {
     Command::new("action")
@@ -12,11 +12,29 @@ pub fn cmd() -> Command {
             .aliases(["i", "show"])
             .arg_required_else_help(true)
             .arg(
-                arg!(<ACTION_NAME> "Name of the ROS action to get info (e.g. '/fibonacci')")
+                Arg::new("action_name")
+                .help("Name of the ROS action to get info (e.g. '/fibonacci')")
                 .required(true)
+                .value_name("ACTION_NAME")
             )
-            .arg( arg!(-t --types "Additionally show the action type") )
-            .arg( arg!(-c --count "Only display the number of action clients and action servers") )
+            .arg(
+                Arg::new("show_types")
+                .short('t')
+                .long("show-types")
+                .aliases(&["show_types", "types"])
+                .help("Additionally show the action type")
+                .action(ArgAction::SetTrue)
+                .conflicts_with("count_actions")
+            )
+            .arg(
+                Arg::new("count_actions")
+                .short('c')
+                .long("count-actions")
+                .aliases(&["count_actions", "count"])
+                .help("Only display the number of actions discovered")
+                .action(ArgAction::SetTrue)
+                .conflicts_with("show_types")
+            )
         )
         .subcommand(
             Command::new("list")
--- src/commands/action/mod.rs
+++ src/commands/action/mod.rs
@@ -1,1 +1,16 @@
 pub mod list;
+pub mod info;
+
+use clap::ArgMatches;
+
+pub fn handle(matches: ArgMatches){
+    match matches.subcommand() {
+        Some(("info", args)) => {
+            info::handle(args.clone());
+        }
+        Some(("list", args)) => {
+            list::handle(args.clone());
+        }
+        _ => unreachable!("UNREACHABLE"),
+    }
+}
