db9ae60d27e47e20d494f67c93868ef6a77cc03e
Trim Bresilla <trim.bresilla@gmail.com>
2023-10-16 23:37:17 +0200

    feat: finished `roc node list`
    - Implemented new `list` command for the `node` module
    - Added `run_command` function to execute the command
    - Handled the stdout and printed the output of the spawned process
    - Created `handle` function to call the necessary functions for the command execution
    - Added new `info` command file in the `node` module
    - Updated the `node` command file to include `list` and `info` modules
    - Made changes to the `arguments/node.rs` file to add new arguments for both `list` and `info` subcommands
    - Reordered the handling of subcommands in the `commands/mod.rs` file

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -18,17 +18,20 @@ use clap::ArgMatches;
 
 pub fn handle(matches: ArgMatches) {
     match matches.subcommand() {
-        Some(("topic", submatch)) => {
-            topic::handle(submatch.clone());
-        }
         Some(("action", submatch)) => {
             action::handle(submatch.clone());
+        }
+        Some(("topic", submatch)) => {
+            topic::handle(submatch.clone());
         }
         Some(("service", submatch)) => {
             service::handle(submatch.clone());
         }
         Some(("param", submatch)) => {
             param::handle(submatch.clone());
+        }
+        Some(("node", submatch)) => {
+            node::handle(submatch.clone());
         }
         _ => unreachable!("UNREACHABLE"),
     };
